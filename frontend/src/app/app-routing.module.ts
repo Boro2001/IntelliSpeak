b2427bd6aa178a19b3c998e0f57a5eae9c27a0ad
plgborowiczm <mikolajborowicz7@gmail.com>
2023-06-25 07:29:25 +0200

    dodanie logowania, wrzucania zarządzania plikami

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {VerifyEmailComponent} from "./component/verify-email/verify-email.component";
+import {ForgotPasswordComponent} from "./components/forgot-password/forgot-password.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {FileUploadComponent} from "./components/file-upload/file-upload.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'file-upload', pathMatch:'full'},
+  {path: 'login', component : LoginComponent},
+  {path: 'dashboard', component : DashboardComponent},
+  {path: 'register', component : RegisterComponent},
+  {path: 'verify-email', component : VerifyEmailComponent},
+  {path: 'forgot-password', component : ForgotPasswordComponent},
+  {path : 'file-upload', component: FileUploadComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
